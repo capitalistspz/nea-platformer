502ebbb35be1ae3f4e55b33491c1349b60e8c219
capitalistspz <amponsahperez@gmail.com>
2022-02-14 21:50:04 +0000

    Added draw method and comments

--- common/src/entities/BaseEntity.cs
+++ common/src/entities/BaseEntity.cs
@@ -8,7 +8,11 @@ namespace common.entities
     {
         private Vector2 _position;
         private Vector2 _velocity;
+        
+        // Whether the entity is rendered or not
         private bool _visible;
+        
+        // Unique identifier for each entity
         public readonly Guid Id;
 
         public BaseEntity(Vector2 position)
@@ -25,16 +29,20 @@ namespace common.entities
             _position = position;
             _visible = true;
         }
-
+        // Physics, inputs etc
         public abstract void Update(GameTime gameTime);
         
+        // Drawing stuff
+        public virtual void Draw(SpriteBatch spriteBatch){}
+        
+        // Obvious
         public void SetPosition(Vector2 position) => _position = position;
         public Vector2 GetPosition() => _position;
-        public void SetVelocity(Vector2 velocity) => _position = velocity;
+        public void SetVelocity(Vector2 velocity) => _velocity = velocity;
         public Vector2 GetVelocity() => _velocity;
         public void AddVelocity(Vector2 deltaVelocity) => _velocity += deltaVelocity;
         public void SetVisibility(bool shouldBeVisible) => _visible = shouldBeVisible;
-        public bool GetVisibility() => _visible;
+        public bool IsVisible() => _visible;
 
     }
 }
