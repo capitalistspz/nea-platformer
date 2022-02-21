55e07d2b4e344fa8073cfaa4633a52a3e8c5181a
capitalistspz <amponsahperez@gmail.com>
2022-02-21 01:22:14 +0000

    Added collisions for player entities

--- common/src/entities/PlayerEntity.cs
+++ common/src/entities/PlayerEntity.cs
@@ -4,6 +4,7 @@ using common.events;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended;
+using MonoGame.Extended.Collisions;
 
 namespace common.entities
 {
@@ -12,15 +13,18 @@ namespace common.entities
         private bool _jump;
         private bool _onGround;
         public readonly string Name;
+        public readonly Vector2 MaxSpeed;
+
         public static Texture2D PlayerTexture;
+        public override IShapeF Bounds { get; }
         public PlayerEntity(Vector2 position, string name) : 
             this(position, Guid.NewGuid()) 
             => (Name, MaxSpeed) = (name, new Size2(8, 8));
 
-        public readonly Vector2 MaxSpeed;
         public PlayerEntity(Vector2 position, Guid guid) : base(position, guid)
         {
             _jump = false;
+            Bounds = new RectangleF(position, new Size2(128, 128));
             SetVisibility(true);
         }
         public override void Update(GameTime gameTime)
@@ -35,14 +39,31 @@ namespace common.entities
             
             var draggedVelocity = oldVelocity * 0.9f;
             SetVelocity(draggedVelocity.LengthSquared() < 0.009f ? Vector2.Zero : draggedVelocity);
+            
             _onGround = false;
             _jump = false;
+            Console.WriteLine($"Name: {Name}\nPosition:  {position}\nVelocity: {GetVelocity()}");
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(PlayerTexture, GetPosition(), Color.White);
         }
+
+        public override void OnCollision(CollisionEventArgs collisionInfo)
+        {
+            if (collisionInfo.Other is PlayerEntity)
+            {
+                Bounds.Position -= collisionInfo.PenetrationVector;
+                collisionInfo.Other.Bounds.Position += collisionInfo.PenetrationVector;
+                
+                //var velocity = GetVelocity() - collisionInfo.PenetrationVector * 0.4f;
+                //SetVelocity(velocity);
+            }
+        }
+
+    
+
         public void ApplyMovement(MovementEventArgs args)
         {
             SetPosition(args.Position);
