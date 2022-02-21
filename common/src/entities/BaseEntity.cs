089059dff7afaa0192c5438d3f19be6c4339c272
capitalistspz <amponsahperez@gmail.com>
2022-02-21 01:14:03 +0000

    BaseEntity inherits from ICollisionActor as an object that can be collided with Added removed attribute so that the EntityManager can detect when to remove an entity

--- common/src/entities/BaseEntity.cs
+++ common/src/entities/BaseEntity.cs
@@ -1,48 +1,56 @@
 using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using MonoGame.Extended.Collisions;
 
 namespace common.entities
 {
-    public abstract class BaseEntity
+    public abstract class BaseEntity : ICollisionActor
     {
-        private Vector2 _position;
         private Vector2 _velocity;
         
         // Whether the entity is rendered or not
         private bool _visible;
+        private bool _removed;
         
         // Unique identifier for each entity
         public readonly Guid Id;
-
+        
         public BaseEntity(Vector2 position)
         {
+            
             _velocity = Vector2.Zero;
-            Id = Guid.NewGuid();
-            _position = position;
             _visible = false;
+            _removed = false;
+            Id = Guid.NewGuid();
+
         }
         public BaseEntity(Vector2 position, Guid guid)
         {
             _velocity = Vector2.Zero;
-            Id = guid;
-            _position = position;
             _visible = true;
+            _removed = false;
+            Id = guid;
+
         }
         // Physics, inputs etc
         public abstract void Update(GameTime gameTime);
         
         // Drawing stuff
         public virtual void Draw(SpriteBatch spriteBatch){}
-        
+        // Marks entity to be destroyed
+        public void RemoveNextUpdate() => _removed = true;
         // Obvious
-        public void SetPosition(Vector2 position) => _position = position;
-        public Vector2 GetPosition() => _position;
+        public void SetPosition(Vector2 position) => Bounds.Position = position;
+        public Vector2 GetPosition() => Bounds.Position;
         public void SetVelocity(Vector2 velocity) => _velocity = velocity;
         public Vector2 GetVelocity() => _velocity;
         public void AddVelocity(Vector2 deltaVelocity) => _velocity += deltaVelocity;
         public void SetVisibility(bool shouldBeVisible) => _visible = shouldBeVisible;
         public bool IsVisible() => _visible;
 
+        public abstract void OnCollision(CollisionEventArgs collisionInfo);
+        public abstract IShapeF Bounds { get; }
     }
 }
