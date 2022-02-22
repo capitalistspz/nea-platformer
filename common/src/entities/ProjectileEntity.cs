29f059ccead7376ed879af9eb6e42d864bbc4c7d
capitalistspz <amponsahperez@gmail.com>
2022-02-22 23:35:30 +0000

    Fixed ProjectileEntity not updating position

--- common/src/entities/ProjectileEntity.cs
+++ common/src/entities/ProjectileEntity.cs
@@ -19,6 +19,8 @@ namespace common.entities
         {
             var deltaTime = (float) (gameTime.ElapsedGameTime.TotalMilliseconds / 10);
             var position = GetPosition() + GetVelocity() * deltaTime;
+
+            SetPosition(position);
         }
     }
 }
