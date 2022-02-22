8834cce9b2d4f94b6ffaf57f76d781b919002cd2
capitalistspz <amponsahperez@gmail.com>
2022-02-22 22:33:14 +0000

    ProjectileEntity takes reference to world Physics implemented

--- common/src/entities/ProjectileEntity.cs
+++ common/src/entities/ProjectileEntity.cs
@@ -5,19 +5,20 @@ namespace common.entities
 {
     public abstract class ProjectileEntity : BaseEntity
     {
-        public ProjectileEntity(Vector2 position) : base(position)
+        public ProjectileEntity(Vector2 position, World world) : base(position,  world)
         {
             SetVisibility(true);
         }
 
-        public ProjectileEntity(Vector2 position, Guid guid) : base(position, guid)
+        public ProjectileEntity(Vector2 position, World world, Guid guid) : base(position, world, guid)
         {
             SetVisibility(true);
         }
 
         public override void Update(GameTime gameTime)
         {
-            
+            var deltaTime = (float) (gameTime.ElapsedGameTime.TotalMilliseconds / 10);
+            var position = GetPosition() + GetVelocity() * deltaTime;
         }
     }
 }
